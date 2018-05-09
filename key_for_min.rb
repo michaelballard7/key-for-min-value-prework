6e0d9b3b4dd6a89fa83b6f734a53a6dc35eb749c
Michael Ballard <ballard.michael9@gmail.com>
2018-05-09 14:16:14 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,6 +8,7 @@ def key_for_min_value(name_hash)
     if value == 0 || value < small_value
       small_value = value 
     end
+    name_hash.key(small_value)
 end
-  name_hash.key(small_value)
+  
 end
