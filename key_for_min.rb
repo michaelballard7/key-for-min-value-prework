c8fa79f67b13344e36ec1828c8d85956c973431a
Michael Ballard <ballard.michael9@gmail.com>
2018-05-09 14:21:01 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   return nil if name_hash == {}
   small_value = 0
   name_hash.each do |key, value|
-    if small_value == 0 || value > small_value
+    if small_value == 0 || small_value > value  
       small_value = value 
     end
   end 
